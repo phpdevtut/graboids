cf3d19edfa9e4edd72443a5f34a3d9daad927295
PHP Developer <railsgeek@yandex.ru>
2021-08-02 01:50:51 +0300

    Initial commit

---
+++ news.php
@@ -0,0 +1,8 @@
+<?php
+
+$layout = file_get_contents('./views/layouts/main.html');
+$content = file_get_contents('./views/news/content.html');
+
+$html = str_replace('%content%', $content, $layout);
+
+echo $html;
