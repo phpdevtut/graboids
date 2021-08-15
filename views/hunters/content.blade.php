a7ef9fab5e29eb876f20fbc20757e2121d83c2aa
PHP Developer <railsgeek@yandex.ru>
2021-08-16 01:14:10 +0300

    Hunters cards

--- views/hunters/content.blade.php
+++ views/hunters/content.blade.php
@@ -1,9 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="myGallery">
+    <div class="card-group">
     @foreach ($hunters as $hunter)
-            <img src="{{ $hunter['src'] }}"/>
+        <div class="card" style="width: 18rem;">
+            <img src="{{ $hunter['src'] }}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Hunter name</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go to hunters profile</a>
+            </div>
+        </div>
     @endforeach
     </div>
     <div class="clear"></div>
